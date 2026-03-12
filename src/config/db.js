a3af08fa7cb915d49634f9ae295b7e76a134050d
Krishmi Hansara <127284933+KrishmiH@users.noreply.github.com>
2026-03-12 20:57:19 +0530

    chore(config): add database connection configuration

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  const uri = process.env.MONGO_URI;
+
+  if (!uri) {
+    throw new Error("MONGO_URI is missing in environment variables");
+  }
+
+  // Optional mongoose config
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(uri);
+
+  console.log("MongoDB connected");
+}
+
+module.exports = connectDB;
